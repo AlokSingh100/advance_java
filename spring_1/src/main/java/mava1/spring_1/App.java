be18b3018d0b101a5783bb1513aae3fbe70be0bd
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2025-02-13 18:53:12 +0530

    Add files via upload

---
+++ spring_1/src/main/java/mava1/spring_1/App.java
@@ -0,0 +1,19 @@
+package mava1.spring_1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      ApplicationContext con=new ClassPathXmlApplicationContext("mava1/spring_1/config.xml");
+      
+      Employee ob=(Employee) con.getBean("emp");
+      System.out.println(ob);
+    }
+}
